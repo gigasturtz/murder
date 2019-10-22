dd08e629013b4d4b03a762aa41e79e1ff34ca480
Nicholas Sturtzel <gigasturtz@Nicholass-MacBook-Pro-7.local>
2019-10-22 14:12:48 -0400

    adds jfInt.lua

--- jfInt.lua
+++ jfInt.lua
@@ -2,24 +2,21 @@
 
 
 function init()
-  output[1].action = pulse(0.5, 10)
-  output[2].slew = .05
-  beat = { 235, 205, 215, 175 }
-  chords = {{ 0, 3, 7 }, { 4, 8, 11 }, { 5, 9, 12 }}
+  ii.jf.mode(1)
+  beat = { 255, 255, 255, 255 }
+  chords = {{ 0, 3, 0 }, { 4, 8, 4 }, { 5, 9, 5 }}
   chordDegree = 1
   currentChord = 1
   pattern = 1
   beatReset = 0
-  clock = metro.init{ event = rhythm_loop(8), time = .1, count = -1 }
+  clock = metro.init{ event = rhythm_loop(8), time = .3, count = -1 }
   clock:start()
 end
 
 function rhythm_loop(beatLength)
   return function(count)
     if math.fmod(bit32.rshift(beat[pattern], beatReset), 2) == 1 then
-      output[1](true)
-      -- play_melody
-      output[2].volts = 2 + (.0833 * (chords[currentChord][chordDegree] + 1))
+      ii.jf.play_note(.0833 * (chords[currentChord][chordDegree]),5)
       chordDegree = chordDegree + 1
       if chordDegree == #chords[currentChord] + 1 then
         chordDegree = 1
