8e0e83cff52291ec34051d3023128ac410b420b7
Nicholas Sturtzel <gigasturtz@Nicholass-MacBook-Pro-7.local>
2019-12-04 23:17:21 -0500

    adds better melody and clock input

--- jfInt.lua
+++ jfInt.lua
@@ -2,28 +2,42 @@
 
 
 function init()
+  ii.pullup(true)
   ii.jf.mode(1)
   beat = { 255, 255, 255, 255 }
-  chords = {{ 0, 3, 0 }, { 4, 8, 4 }, { 5, 9, 5 }}
+  chords = {
+    { 15, 15, 19, 15, 15, 19, 15, 15 },
+    { 14, 15, 17, 14, 15, 17, 14, 15 },
+    { 15, 15, 19, 15, 15, 19, 15, 15 },
+    { 12, 15, 15, 12, 15, 15, 12, 15 },
+    { 10, 15, 15, 10, 15, 15, 10, 15 },
+    { 8, 15, 15, 8, 15, 15, 8, 15 },
+    { 10, 14, 20, 10, 14, 20, 10, 14 },
+    { 10, 14, 20, 10, 14, 20, 10, 14 },
+    { 8, 15, 15, 8, 15, 15, 8, 15 }
+  }
   chordDegree = 1
   currentChord = 1
   pattern = 1
   beatReset = 0
-  clock = metro.init{ event = rhythm_loop(8), time = .3, count = -1 }
-  clock:start()
+  input[1].mode('change', 1.0,0.1,'rising')
+end
+
+input[1].change = function(state)
+  rhythm_loop(8)
 end
 
 function rhythm_loop(beatLength)
-  return function(count)
     if math.fmod(bit32.rshift(beat[pattern], beatReset), 2) == 1 then
       ii.jf.play_note(.0833 * (chords[currentChord][chordDegree]),5)
+      print(chords[currentChord][chordDegree])
       chordDegree = chordDegree + 1
       if chordDegree == #chords[currentChord] + 1 then
         chordDegree = 1
       end
     end
     beatReset = beatReset + 1
-    if beatReset == beatLength + 1 then
+    if beatReset == 8 then
       beatReset = 0
       pattern = pattern + 1
       if pattern == #beat + 1 then
@@ -34,5 +48,4 @@ function rhythm_loop(beatLength)
         currentChord = 1
       end
     end
-  end
 end
