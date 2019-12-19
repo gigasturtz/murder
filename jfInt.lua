54f71025e3079c12678cbbbca4a2d051db2a4f9a
Nicholas Sturtzel <gigasturtz@Nicholass-MacBook-Pro-7.local>
2019-12-19 17:51:56 -0500

    abstracts increment function

--- jfInt.lua
+++ jfInt.lua
@@ -4,6 +4,7 @@
 function init()
   ii.pullup(true)
   ii.jf.mode(1)
+  ii.jf.transpose(-1)
   beat = { 255, 255, 255, 255 }
   chords = {
     { 15, 15, 19, 15, 15, 19, 15, 15 },
@@ -16,10 +17,10 @@ function init()
     { 10, 14, 20, 10, 14, 20, 10, 14 },
     { 8, 15, 15, 8, 15, 15, 8, 15 }
   }
-  chordDegree = 1
-  currentChord = 1
-  pattern = 1
-  beatReset = 0
+  chordDegree = {1}
+  currentChord = {1}
+  pattern = {1}
+  beatReset = {0}
   input[1].mode('change', 1.0,0.1,'rising')
 end
 
@@ -28,24 +29,30 @@ input[1].change = function(state)
 end
 
 function rhythm_loop(beatLength)
-    if math.fmod(bit32.rshift(beat[pattern], beatReset), 2) == 1 then
-      ii.jf.play_note(.0833 * (chords[currentChord][chordDegree]),5)
-      print(chords[currentChord][chordDegree])
-      chordDegree = chordDegree + 1
-      if chordDegree == #chords[currentChord] + 1 then
-        chordDegree = 1
+    if math.fmod(bit32.rshift(beat[pattern[1]], beatReset[1]), 2) == 1 then
+      if chordDegree[1] == 1 then
+        ii.jf.play_note(.0833  * (chords[currentChord[1]][chordDegree[1]] - 12),5)
+      end
+      ii.jf.play_note(.0833 * (chords[currentChord[1]][chordDegree[1]]),5)
+      if incrementValue(chordDegree, #chords[currentChord[1]] + 1) then
+        chordDegree[1] = 1
       end
     end
-    beatReset = beatReset + 1
-    if beatReset == 8 then
-      beatReset = 0
-      pattern = pattern + 1
-      if pattern == #beat + 1 then
-        pattern = 1
+    if incrementValue(beatReset, beatLength) then
+      beatReset[1] = 0
+      if incrementValue(pattern, #beat) then
+        pattern[1] = 1
       end
-      currentChord = currentChord + 1
-      if currentChord == #chords + 1 then
-        currentChord = 1
+      if incrementValue(currentChord, #chords) then
+        currentChord[1] = 1
       end
     end
+end
+
+function incrementValue(value, max)
+  value[1] = value[1] + 1
+  if value[1] == max then
+    return true
+  end
+  return false
 end
